5efcc9888b462da6987ffcf7d3ee3c0a42923f57
javida1492 <javida1492@gmail.com>
2024-12-01 18:21:50 -0500

    AoC day1 part 2

--- day1.js
+++ day1.js
@@ -1,47 +1,62 @@
 const fs = require("fs")
 const readline = require("readline")
 
+const processFile = async (col1, col2) => {
+  //Create file input stream
+  return new Promise((resolve, reject) => {
+    const readInterface = readline.createInterface({
+      input: fs.createReadStream("day1Input.txt"),
+    })
+
+    //Process input file
+    readInterface.on("line", (line) => {
+      const values = line.split(" ")
+      col1.push(values[0])
+      col2.push(values[values.length - 1])
+    })
+
+    //Close the file
+    readInterface.on("close", () => {
+      console.log("File processing complete")
+      resolve()
+    })
+    // Handle errors
+    readInterface.on("error", (err) => {
+      reject(err)
+    })
+  })
+}
+
 const solution = async () => {
   //   const col1 = ["3", "4", "2", "1", "3", "3"]
   //   const col2 = ["4", "3", "5", "3", "9", "3"]
   const col1 = []
   const col2 = []
+  const col2Map = new Map()
 
-  //Create file input stream
-  const processFile = (col1, col2) => {
-    return new Promise((resolve, reject) => {
-      const readInterface = readline.createInterface({
-        input: fs.createReadStream("day1Input.txt"),
-      })
-
-      //Process input file
-      readInterface.on("line", (line) => {
-        const values = line.split(" ")
-        col1.push(values[0])
-        col2.push(values[values.length - 1])
-      })
-
-      //Close the file
-      readInterface.on("close", () => {
-        console.log("File processing complete")
-        resolve()
-      })
-      // Handle errors
-      readInterface.on("error", (err) => {
-        reject(err)
-      })
-    })
-  }
   await processFile(col1, col2)
 
-  col1.sort((a, b) => a - b)
-  col2.sort((a, b) => a - b)
-
-  let distances = 0
-  for (let i = 0; i < col1.length; i++) {
-    distances += Math.abs(col1[i] - col2[i])
+  for (const num of col2) {
+    if (!col2Map.has(num)) {
+      col2Map.set(num, 0)
+    }
+    col2Map.set(num, col2Map.get(num) + 1)
   }
-  return distances
+
+  let similarityScore = 0
+  for (const num of col1) {
+    similarityScore += num * (col2Map.has(num) ? col2Map.get(num) : 0)
+  }
+
+  //   col1.sort((a, b) => a - b)
+  //   col2.sort((a, b) => a - b)
+
+  //   let distances = 0
+  //   for (let i = 0; i < col1.length; i++) {
+  //     distances += Math.abs(col1[i] - col2[i])
+  //   }
+  //   return distances
+  return similarityScore
 }
 
-console.log(solution())
+solution().then((result) => console.log(result))
